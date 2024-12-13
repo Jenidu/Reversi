4426fde4a01ff7b5430fcf2ee62f7d978a33126d
Jenidu <jenidude7@gmail.com>
2024-12-13 14:27:43 +0100

    Beginnetje

--- App/Reversi/Program.cs
+++ App/Reversi/Program.cs
@@ -2,15 +2,19 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
-/* Grootte en startposities*/
-int[] Venster_Grootte = { 760, 545 }; /* Venster grootte */
-int[] StartBM = { 15, 130 };  /* Start positie van bitmap, relatief tot de venster */
-int[] BM_Grootte = { 400, 400 };  /* Bitmap grootte */
+/* Grootte en startposities */
+int[] Venster_Grootte = {760, 545}; /* Venster grootte */
+int[] StartBM = {15, 130};  /* Start positie van bitmap, relatief tot de venster */
+int[] BM_Grootte = {400, 400};  /* Bitmap grootte */
 
 /* Tekst boxen grootte en posities */
-int[] StartVensterTekst = { 10, 15 };
-int[] TekstBox_PosVerschil = { 0, 25 };
+int[] StartVensterTekst = {10, 15};
+int[] TekstBox_PosVerschil = {0, 25};
+
+
+Color[] steen_kleuren = {Color.Red, Color.Blue};  /* Steen kleuren */
 
 /* Scherm venster */
 Form scherm = new Form();
@@ -22,7 +26,7 @@ scherm.ClientSize = new Size(Venster_Grootte[0], Venster_Grootte[1]);
 Label hello_world = new Label();
 
 /* Knoppen */
-Button ga_knop = new Button();
+Button help_knop = new Button();
 
 /* Bitmaps */
 Bitmap plaatje = new Bitmap(BM_Grootte[0], BM_Grootte[1]);
@@ -33,9 +37,17 @@ Label label_plaatje = new Label();
 /* Dropdown */
 ComboBox nieuwspel_opties = new ComboBox();
 
+/* Spelsituatie */
+int[,] game_state = new int[16, 16];  /* -1: buiten veld, 0: van niemand, 1: van speler 1, 2: van speler 2 */
+int tile_size, tile_dim;  /* Grid properties */
+
+
+
 opstartVenster();
 
 nieuwspel_opties.SelectedIndexChanged += nieuwSpelOpties;  /* Dropdown veranderd */
+
+help_knop.Click += helpLaden;  /* Er is op 'help' geklikt */
 
 Application.Run(scherm);
 
@@ -55,38 +67,87 @@ void opstartVenster()
     hello_world.Text = "midden x:";
 
     /* Knop aanmaken voor input gedeelte */
-    scherm.Controls.Add(ga_knop);
-    ga_knop.Location = new Point(225, 90);
-    ga_knop.Size = new Size(40, 25);
-    ga_knop.Text = "Ga!";
+    scherm.Controls.Add(help_knop);
+    help_knop.Location = new Point(250, 10);
+    help_knop.Size = new Size(40, help_knop.Height);
+    help_knop.Text = "help";
 
     /* Dropdown */
     scherm.Controls.Add(nieuwspel_opties);
-    nieuwspel_opties.Location = new Point(50, 50);
+    nieuwspel_opties.Location = new Point(120, 10);
     nieuwspel_opties.Text = "Nieuw spel";
 
-    nieuwspel_opties.Items.Add(new DropdownItem("Start nieuw 4x4 spel", 1));
-    nieuwspel_opties.Items.Add(new DropdownItem("Start nieuw 6x6 spel", 2));
-    nieuwspel_opties.Items.Add(new DropdownItem("Start nieuw 8x8 spel", 3));
-    nieuwspel_opties.Items.Add(new DropdownItem("Start nieuw 10x10 spel", 4));
-
+    nieuwspel_opties.Items.Add(new DropdownItem("Start nieuw 4x4 spel", "4x4"));
+    nieuwspel_opties.Items.Add(new DropdownItem("Start nieuw 6x6 spel", "6x6"));
+    nieuwspel_opties.Items.Add(new DropdownItem("Start nieuw 8x8 spel", "8x8"));
+    nieuwspel_opties.Items.Add(new DropdownItem("Start nieuw 10x10 spel", "10x10"));
 }
 
 void nieuwSpelOpties(object o, EventArgs e)
 {
-    Console.WriteLine($"You selected: {nieuwspel_opties.SelectedItem}");
+    if (nieuwspel_opties.SelectedItem is DropdownItem selectedItem)  /* Een item is geselecteerd */
+    {
+        Console.WriteLine($"Selected type: {selectedItem.Type}, Text: {selectedItem.Text}");
+        if (selectedItem.Type == "4x4")
+            createNewBoard(4);
+        else if (selectedItem.Type == "6x6")
+            createNewBoard(6);
+        else if (selectedItem.Type == "8x8")
+            createNewBoard(8);
+        else if (selectedItem.Type == "10x10")
+            createNewBoard(10);
+        else
+            Console.WriteLine($"Incorrect item selected: type: {selectedItem.Type}, Text: {selectedItem.Text}");
+    }
 }
 
+void createNewBoard(int size){
 
+    tile_dim = size;  /* Nieuwe hoogte en breedte van het speelveld */
+
+    for (int i = 0; i < 16; i++)  /* Reset board */
+    {
+        for (int j = 0; j < 16; j++)
+            game_state[i, j] = -1;
+    }
+
+
+
+    updateStenenTeller();
+}
+
+void helpLaden(object o, EventArgs e){
+
+}
+
+void updateStenenTeller(){
+
+    int[] stenen_teller = {0, 0};
+
+    for (int i = 0; i < 16; i++)  /* Reset board */
+    {
+        for (int j = 0; j < 16; j++)
+        {
+            if (game_state[i, j] == 1)
+                stenen_teller[0]++;
+            else if (game_state[i, j] == 2)
+                stenen_teller[1]++;
+        }
+    }
+    //Diplay stenen
+
+}
+
+//MessageBox.Show($"Speler {} heeft gewonnen!");
 
 public class DropdownItem
 {
     public string Text { get; set; }
-    public int Id { get; set; }
+    public string Type { get; set; }
 
-    public DropdownItem(string text, int id){
+    public DropdownItem(string text, string type){
         Text = text;
-        Id = id;
+        Type = type;
     }
 
     public override string ToString(){
